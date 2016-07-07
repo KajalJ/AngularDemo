facc1789ed3e142316fea25938de598b8e620e3d
Joe Johnson <joe.wendell.johnson@gmail.com>
2016-07-07 15:08:22 -0500

    Updated to use async get

--- AngularDemo/app.js
+++ AngularDemo/app.js
@@ -1,56 +1,13 @@
-﻿var gems =[ {
-    name: 'Dodecahedron',
-    price: 2.95,
-    description: 'Some gems have qualities beyond their lustre, beyond their shine.  Dodecahedron is one of those gems.',
-    canPurchase: true,
-    soldOut: false,
-    images: [
-        {
-            full: 'dodecahedron.png',
-            thumb: 'dodecahedron_thumb.png'
-        },
-        {
-            full: 'dodecahedron2.jpg',
-            thumb: 'dodecahedron2_thumb.jpg'
-        }
-    ],
+﻿var module = angular.module('sparky', ['store-products']);
 
-    reviews:[
-    {stars: 5,
-        body: "I love this product",
-        author: "Joe@Thomas.com"
-    },{
-        star:1,
-        body:"This product isn't even real",
-        author:"tim@hater.com"
-    }]
-} ,{
-    name: 'Pentagonal',
-    price: 5.95,
-    description: 'I am a gem shaped as a pentagon',
-    canPurchase: true,
-    soldOut: false
-}]
+  module.controller('StoreController', ['$http', function($http){
+      var store = this;
+      store.products = [];
 
-var module = angular.module('sparky', []);
-
-  module.controller('StoreController', function ($scope) {
-
-        this.products = gems;
-        //alert("Welcome, Joe!");
-  });
-
-  module.controller('PanelController', function ($scope) {
-      this.tab = 1;
-
-      this.updateTab = function (newTab) {
-          this.tab = newTab;
-      }
-
-      this.isSelected = function (currentTab) {
-          return this.tab === currentTab;
-      }
-  });
+      $http.get('/products.json').success(function (data) {
+          store.products = data;
+      })
+  }]);
 
   module.controller('ReviewController', function ($scope) {
       this.review = {};
---
+++ AngularDemo/product.js
@@ -0,0 +1,27 @@
+﻿var module = angular.module('store-products', []);
+
+module.directive('productDetails', function () {
+    return {
+        restrict: 'E',
+        templateUrl: 'product-details.html'
+    };
+})
+
+module.directive('productPanels', function () {
+    return {
+        restrict: 'E',
+        templateUrl: 'product-panels.html',
+        controller: function () {
+            this.tab = 1;
+
+            this.updateTab = function (newTab) {
+                this.tab = newTab;
+            }
+
+            this.isSelected = function (currentTab) {
+                return this.tab === currentTab;
+            }
+        },
+        controllerAs: 'panel'
+    }
+})
